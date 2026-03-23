30a3891e9c97a178833a2e91889567a5b3753e56
sigisitar30 <sigisitar@gmail.com>
2026-03-23 22:55:28 +0100

    Popravljeno: deljenje na X

--- script.js
+++ script.js
@@ -943,7 +943,7 @@ function initShareUi() {
                 calculator: cfg.id || undefined,
             });
             const shareUrl = buildShareUrl(cfg);
-            const intent = new URL("https://x.com/intent/post");
+            const intent = new URL("https://twitter.com/intent/tweet");
             intent.searchParams.set("text", getShareText());
             intent.searchParams.set("url", shareUrl);
             openPopup(intent.toString());
